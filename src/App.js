0084db1ca6e58d03160e65d21762c7a977c7c5e2
Savannah Ricks <savannahricksofficial123@gmail.com>
2023-11-15 15:30:45 -0500

    adding list of people better

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import cat2 from './IMG_2972.JPG'
 import cat3 from './IMG_1249.JPG'
 import './App.css';
 import { useState } from 'react';
+import { people } from './data.js';
+import { getImageUrl } from './util.js';
 
 function App() {
   return (
@@ -59,47 +61,12 @@ function MyButton() {
 }
 
 
-    const people = [{
-        id: 0,
-        name: 'Creola Katherine Johnson',
-        profession: 'mathematician',
-        accomplishment: 'spaceflight calculations',
-        imageId: 'MK3eW3A'
-    }, {
-        id: 1,
-        name: 'Mario José Molina-Pasquel Henríquez',
-        profession: 'chemist',
-        accomplishment: 'discovery of Arctic ozone hole',
-        imageId: 'mynHUSa'
-    }, {
-        id: 2,
-        name: 'Mohammad Abdus Salam',
-        profession: 'physicist',
-        accomplishment: 'electromagnetism theory',
-        imageId: 'bE7W1ji'
-    }, {
-        id: 3,
-        name: 'Percy Lavon Julian',
-        profession: 'chemist',
-        accomplishment: 'pioneering cortisone drugs, steroids and birth control pills',
-        imageId: 'IOjWm71'
-    }, {
-        id: 4,
-        name: 'Subrahmanyan Chandrasekhar',
-        profession: 'astrophysicist',
-        accomplishment: 'white dwarf star mass calculations',
-        imageId: 'lrWQx8l'}];
 
 
 
-    function getImageUrl(person) {
-        return (
-            'https://i.imgur.com/' +
-            person.imageId +
-            's.jpg'
-        );
-    }
-    function List() {
+
+
+    function List1() {
         const listItems = people.map(person =>
             <li key={person.id}>
                 <img
@@ -121,6 +88,25 @@ function MyButton() {
         );
     }
 
+
+
+function List() {
+
+    const listItems = people.map(person =>
+        <li>
+            <img
+                src={getImageUrl(person)}
+                alt={person.name}
+            />
+            <p>
+                <b>{person.name}:</b>
+                {' ' + person.profession + ' '}
+                known for {person.accomplishment}
+            </p>
+        </li>
+    );
+    return <ul>{listItems}</ul>;
+}
 
 
 
